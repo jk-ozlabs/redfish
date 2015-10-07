37017003b87b074a00e29692787826ff0e4477e8
Jeremy Kerr <jk@ozlabs.org>
2015-10-07 11:09:56 +0800

    Initial commit

---
+++ create-models.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+
+import sys
+import lxml.etree
+
+ns = {
+    'edmx': 'http://docs.oasis-open.org/odata/ns/edmx',
+    'edm': 'http://docs.oasis-open.org/odata/ns/edm',
+}
+
+types = {}
+
+required_types = [
+    'ServiceRoot.1.0.0.ServiceRoot',
+    'ComputerSystem.1.0.0.ComputerSystem',
+]
+
+def qualified_classname_to_python(name):
+    return name.replace('.', '_')
+
+class Property(object):
+    prop_class = 'odata.ODataProperty'
+    collection_class = 'odata.ODataCollectionProperty'
+
+    def __init__(self, name):
+        self.name = name
+        self.nav = False
+        self.collection_type = None
+        self.cls = self.prop_class
+
+    def render(self):
+        attrs = {}
+        if self.nav:
+            attrs['nav'] = 'True'
+        if self.collection_type:
+            cls = self.collection_class
+            attrs['type'] = repr(self.collection_type)
+        else:
+            cls = self.prop_class
+        attr_string = ''.join([', %s=%s' % (k, v) for (k, v) in attrs.items() ])
+        return "%s('%s'%s)" % (cls, self.name, attr_string)
+
+    @classmethod
+    def parse(cls, elem):
+        prop = cls(elem.get('Name'))
+        t = elem.get('Type')
+        if elem.tag == ('{%s}NavigationProperty' % ns['edm']):
+            prop.nav = True
+        if t.endswith('Collection'):
+            prop.collection_type = t
+        return prop
+
+class EntityType(object):
+    odata_class = 'odata.ODataObject'
+
+    def __init__(self, name):
+        self.parent = None
+        self.name = name
+        self.properties = []
+        self.navigation_properties = []
+        self.rendered = False
+
+    def merge(self, other):
+        if not other:
+            return
+        assert(other.name == self.name)
+        self.properties += other.properties
+
+    def render(self):
+        global types
+        if self.rendered:
+            return
+        parent_name = self.odata_class
+        if self.parent:
+            parent = types.get(self.parent, None)
+            if parent is None:
+                raise Exception("No such type %s" % self.parent)
+            parent.render()
+            parent_name = qualified_classname_to_python(self.parent)
+        print "class %s(%s):" % (qualified_classname_to_python(self.name),
+                parent_name)
+        for prop in self.properties:
+            print '\t%s = %s' % (prop.name.lower(), prop.render())
+        for prop in self.navigation_properties:
+            print '\t%s = %s' % (prop.name.lower(), prop.render())
+
+        print
+        print '\tclass Meta:'
+        print '\t\ttype = \'%s\'' % (self.name)
+        print
+        self.rendered = True
+
+
+    @classmethod
+    def parse(cls, namespace, elem):
+        et = cls(namespace + '.' + elem.get('Name'))
+        et.parent = elem.get('BaseType')
+        for prop_elem in elem.xpath('edm:Property', namespaces=ns):
+            prop = Property.parse(prop_elem)
+            et.properties.append(prop)
+        for prop_elem in elem.xpath('edm:NavigationProperty', namespaces=ns):
+            prop = Property.parse(prop_elem)
+            et.properties.append(prop)
+        return et
+
+
+def handle_dataservice(ds):
+    for schema in ds.xpath('edm:Schema', namespaces=ns):
+        schema_namespace = schema.get('Namespace')
+        for et in schema.xpath('edm:EntityType', namespaces=ns):
+            et = EntityType.parse(schema_namespace, et)
+#et.merge(types.get(et.name, None))
+            types[et.name] = et
+
+
+
+def process_file(filename):
+    tree = lxml.etree.parse(open(filename))
+    for ds in tree.xpath('/edmx:Edmx/edmx:DataServices', namespaces=ns):
+        handle_dataservice(ds)
+
+def main():
+
+    print 'import odata'
+    print
+
+    for arg in sys.argv[1:]:
+        process_file(arg)
+
+    for t in required_types:
+        types[t].render()
+        
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ odata.py
@@ -0,0 +1,106 @@
+
+
+import collections
+import json
+
+def join_ids(a, b):
+    c = ''
+    if not a.endswith('/'):
+        c = '/'
+    return a + c + b
+
+class ODataProperty(object):
+    def __init__(self, name, nav=False):
+        self.name = name
+        self.python_name = name.lower()
+        self.nav = nav
+
+    def __repr__(self):
+        return '<Property %s>' % self.name
+
+    def json_repr(self, obj):
+        if self.nav:
+            return { '@odata.id': join_ids(obj._odata_id, self.name) }
+        return getattr(obj, self.python_name, None)
+
+class ODataCollectionProperty(ODataProperty):
+
+    def __init__(self, name, nav=False, type=None):
+        self.name = name
+        self.python_name = name.lower()
+        self.nav = nav
+        self.collection_type = type
+
+    def __repr__(self):
+        return '<CollectionProperty %s>' % self.name
+
+class ODataObject(object):
+
+    def __init__(self):
+        self._odata_init()
+
+    def _odata_init(self):
+        self._meta = getattr(self, 'Meta')()
+        self._meta.properties = collections.OrderedDict()
+        for name in dir(self):
+            if name == '_odata_id':
+                continue
+            val = getattr(self, name)
+            if isinstance(val, ODataProperty):
+                self._meta.properties[name] = val
+                if isinstance(val, ODataCollectionProperty):
+                    collection = ODataCollection()
+                    collection._meta.type = val.collection_type
+                    setattr(self, name, collection)
+                else:
+                    setattr(self, name, None)
+
+    def assign_ids(self, parent, ctx):
+        if getattr(self, '_odata_id', None) is None:
+            self._odata_id = join_ids(parent._odata_id, ctx)
+            self.id = ctx
+        for prop in self._meta.properties.values():
+            value = getattr(self, prop.python_name, None)
+            if isinstance(value, ODataObject):
+                value.assign_ids(self, prop.name)
+
+    def to_json_dict(self):
+        d = {}
+        for prop in self._meta.properties.values():
+            val = prop.json_repr(self)
+            if val is None:
+                continue
+            d[prop.name] = val
+        return d
+
+class ODataCollection(ODataObject, list):
+
+    def assign_ids(self, parent, ctx):
+        self._odata_id = join_ids(parent._odata_id, ctx)
+        for key, value in enumerate(self):
+            if isinstance(value, ODataObject):
+                value.assign_ids(self, str(key))
+
+    def to_json_dict(self):
+        return {
+            'Members@odata.count': self.__len__(),
+            'Members': [
+                { '@odata.id': v._odata_id for v in self }
+             ]
+        }
+
+    class Meta:
+        pass
+
+def odata_json_encode(obj, context=None):
+    def json_handler(obj):
+        if isinstance(obj, ODataObject):
+            return obj.to_json_dict()
+        raise TypeError(repr(obj) + " is not JSON serialisable")
+    d = {
+        '@odata.type': '#' + obj._meta.type,
+    }
+    if context is not None:
+        d.update({'@odata.context': '/redfish/v1/$metadata#' + context})
+    d.update(obj.to_json_dict())
+    return json.dumps(d, indent=2, default=json_handler) + '\n'
